399478df6aeefe28ae385b82104192c7bb3dfdc4
AlejandraRV91 <alejandraramirez@pursuit.org>
2023-07-15 12:27:36 -0500

    progress: Basic CRUD Data and Operations

--- server.js
+++ server.js
@@ -1,35 +1,62 @@
+/** @format */
+
 let express = require("express");
 let app = express();
 require("dotenv").config();
-let cors=require("cors")
-app.use(cors())
+let cors = require("cors");
+
+//middlewares
+app.use(cors());
+app.use(express.json());
 
 let port = process.env.PORT;
 
-app.post("/", (req, res) => {
-    
+//data
+let transactions = [
+	{
+		id: 1,
+		item_name: "income",
+		amount: 1000,
+		date: "2023-07-14",
+		from: "employer",
+		category: "income",
+	},
+	{
+		id: 2,
+		item_name: "cat food",
+		amount: -20,
+		date: "2023-07-15",
+		from: "pet store",
+		category: "pets",
+	},
+];
+
+//create
+app.post("/transactions", (req, res) => {
+	const { item_name, amount, date, from, category } = req.body;
+	const id = Date.now();
+	const newTransaction = { id, item_name, amount, date, from, category };
+	transactions.push(newTransaction);
 	res.sendStatus(201);
 });
 
-
-app.get("/", (req, res) => {
-	res.json();
+//read
+app.get("/transactions", (req, res) => {
+	res.json(transactions);
+});
+app.get("/transactions/:id", (req, res) => {
+	const { id } = req.params;
+	const transaction = transactions.find((t) => t.id === Number(id));
+	if (!transaction) {
+		res.sendStatus(404);
+	} else {
+		res.json(transaction);
+	}
 });
 
-
-app.get("//:id", (req, res) => {
-	res.json();
-});
-
-
-app.put("/", (req, res) => {
-    
-	res.sendStatus(201);
-});
-
-app.delete("/", (req, res) => {
-    
-	res.sendStatus(201);
+//error 404
+app.use((req, res) => {
+	res.sendStatus(404);
 });
 
 app.listen(port, () => {
